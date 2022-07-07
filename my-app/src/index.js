c232b5593129d9c5cedfe68cab39c700da86239c
claudineivolpato <claudineivolpato@gmail.com>
2022-07-06 21:48:52 -0300

    lab02

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -6,17 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-function tick() {
-  const element = (
-    <div>
-      <h1>Olá, Valdair!</h1>
-      <h2>Hora agora: {new Date().toLocaleTimeString()}.</h2>
-    </div>
-  );
-  root.render(element, document.getElementById('root'));
-}
 
-setInterval(tick, 1000);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

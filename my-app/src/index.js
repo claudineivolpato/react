ada02bcec6ba5e0a89f2de40ab4d04589c5b7c79
claudineivolpato <claudineivolpato@gmail.com>
2022-07-06 21:42:33 -0300

    ssss

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -6,16 +6,20 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const nome = "Claudinei"
-const element = <h1>Ola {nome}</h1>
+function tick() {
+  const element = (
+    <div>
+      <h1>Olá, Valdair!</h1>
+      <h2>Hora agora: {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  root.render(element, document.getElementById('root'));
+}
 
-root.render(
- // <React.StrictMode>
-    element
- // </React.StrictMode>
-);
+setInterval(tick, 1000);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();

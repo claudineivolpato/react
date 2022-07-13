397fb62676c24c922074f9f119f1092ef32f7fcc
claudineivolpato <claudineivolpato@gmail.com>
2022-07-12 21:33:26 -0300

    xxx

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import HeaderComponent from './components/HeaderComponent';
 import FooterComponent from './components/FooterComponent';
 import ListContactComponent from './components/ListContactComponent';
+import CreateContactComponent from './components/CreateContactComponent';
 
 function App() {
   return (
     <div>
       <HeaderComponent />
       <ListContactComponent />
+      <CreateContactComponent />
       <FooterComponent />
     </div>
   );
---
+++ my-app/src/components/CreateContactComponent.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+class CreateContactComponent extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div>
+                <h2 className="text-center">Cadastro de Contatos</h2>
+            </div>
+        )
+    }
+}
+
+
+export default CreateContactComponent;

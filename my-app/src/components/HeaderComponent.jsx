fd73a5d214dbe727bed8beb1156b7ccd7c3ba5c9
claudineivolpato <claudineivolpato@gmail.com>
2022-07-12 21:11:15 -0300

    crud

---
+++ my-app/src/components/HeaderComponent.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+class HeaderComponent extends React.Component {
+    constructor(props) {
+        super(props)
+
+    }
+
+    render() {
+        return (
+            <div>
+                <header>
+                    <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+                        <div><a href="#" className="navbar-brand">Menu Gestao de Produtos</a></div>
+                    </nav>
+                </header>
+            </div>
+        )
+    }
+}
+
+export default HeaderComponent;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,42 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import ListaService from './services/ListaService';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-class Opa extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = { ativo: false }
-    console.log(this.state);
-  }
-
-  componentDidMount() {  // executa antes de renderizar
-    console.log(this.state.ativo);
-  }
-
-  render() {
-
-    let className="h1";
-    if(this.state.ativo == true){
-      className += " ativo"
-    } else {
-      className += " inativo"
-    }
-
-    return (
-      <div>
-        <h1 className={className}>Opaaaa</h1>
-        <button className="btn btn-primary" id='btnTeste'>Teste</button>
-      </div>
-    )
-  }
-}
 
 root.render(
-  <Opa />
+  <React.StrictMode>
+    <App />
+
+  </React.StrictMode>
 )
 
 // If you want to start measuring performance in your app, pass a function

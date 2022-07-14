ddd1ca210149c62d021b6eaff43d2dcfbeecee88
claudineivolpato <claudineivolpato@gmail.com>
2022-07-13 21:45:53 -0300

    cccc

--- my-app/src/components/CreateContactComponent.jsx
+++ my-app/src/components/CreateContactComponent.jsx
@@ -3,12 +3,42 @@ import React from "react";
 class CreateContactComponent extends React.Component {
     constructor(props) {
         super(props)
+
+        this.contactList = this.contactList.bind(this);
+    }
+
+    contactList(){
+        this.props.history.push('/')
     }
 
     render() {
         return (
             <div>
-                <h2 className="text-center">Cadastro de Contatos</h2>
+                <div className="container">
+                    <div className="card col-md-6 offset-md-3">
+                        <h2 className="text-center">Cadastro de Contatos</h2>
+                        <div className="card-body">
+                            <form>
+                                <div className="form-group">
+                                    <label>Código</label>
+                                    <input placeholder="Código" name="codigo" required className="form-control"></input>
+                                </div>
+                                <div className="form-group">
+                                    <label>Nome</label>
+                                    <input placeholder="Nome" name="nome" required className="form-control"></input>
+                                </div>
+                                <div className="form-group">
+                                    <label>Telefone</label>
+                                    <input placeholder="Telefone" name="telefone" required className="form-control"></input>
+                                </div>
+
+                                <button className="btn btn-success">Salvar</button>
+                                <button className="btn btn-secondary" onClick={this.contactList}>Cancelar</button>
+
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
